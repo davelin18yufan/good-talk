d6bc1b07abd2fb890e822ff6ef9f8787ef76002b
davelin18yufan <davelin30630@gmail.com>
2024-05-31 17:17:27 +0800

    remove secret

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -1,18 +1,25 @@
 import { NextResponse, type NextRequest } from "next/server"
 import { parseBody } from "next-sanity/webhook"
 import { revalidatePath } from "next/cache"
-import { SanityDocument, toPlainText } from "next-sanity"
+import { toPlainText, type PortableTextBlock } from "next-sanity"
+import { Slug } from "@/sanity.types"
+
+interface Webhook {
+  _type: string
+  title:string
+  content: PortableTextBlock[]
+  slug: Slug
+}
 
 // request comes from sanity webhook if an new post created
 // line-notify
 export async function POST(request: NextRequest) {
-  // TODO:transfer request content to formData
   try {
     //* Webhook
     // parse body from sanity webhook
-    const { body, isValidSignature } = await parseBody<SanityDocument>(
+    const { body, isValidSignature } = await parseBody<Webhook>(
       request,
-      process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET
+      // process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET
     )
 
     if (!isValidSignature) {
@@ -37,7 +44,7 @@ export async function POST(request: NextRequest) {
       headers: {
         // unnecessary content configuration, cause browser will automatically set a boundary parameter for data split
         // "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_LINE_NOTIFY_TOKEN!}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_LINE_NOTIFY_TOKEN}`,
       },
       body: formData,
     })
