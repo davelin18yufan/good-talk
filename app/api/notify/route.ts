fe64095d0db4674225680ef7484086672a6874b0
davelin18yufan <davelin30630@gmail.com>
2024-05-31 16:38:30 +0800

    [feat] add webhook and testing

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -1,31 +1,58 @@
 import { NextResponse, type NextRequest } from "next/server"
-import { toPlainText } from "next-sanity"
 import { parseBody } from "next-sanity/webhook"
+import { revalidatePath } from "next/cache"
+import { SanityDocument, toPlainText } from "next-sanity"
 
 // request comes from sanity webhook if an new post created
 // line-notify
 export async function POST(request: NextRequest) {
   // TODO:transfer request content to formData
-  const { isValidSignature, body } = await parseBody(
-    request,
-    process.env.SANITY_WEBHOOK_CREATE_SECRET
-  )
+  try {
+    //* Webhook
+    // parse body from sanity webhook
+    const { body, isValidSignature } = await parseBody<SanityDocument>(
+      request,
+      process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET
+    )
 
-  if(!isValidSignature) return NextResponse.json({message:'Bad Request'})
+    if (!isValidSignature) {
+      return new Response("Invalid Signature", { status: 401 })
+    }
 
-  console.log(body)
-  // const formData = await request.formData()
-  
-  // const res = await fetch("https://notify-api.line.me/api/notify", {
-  //   method: "POST",
-  //   headers: {
-  //     //* unnecessary content configuration, cause browser will automatically set a boundary parameter for data split
-  //     // "Content-Type": "multipart/form-data",
-  //     Authorization: `Bearer ${process.env.NEXT_PUBLIC_LINE_NOTIFY_TOKEN!}`,
-  //   },
-  //   body: formData,
-  // })
+    if (!body?._type) { // every successful sanity request has _type
+      return new Response("Bad Request", { status: 400 })
+    }
 
-  // const data = await res.json()
-  // return NextResponse.json(data)
+    //* Line Notify 
+    const { title, content, slug } = body
+    const formData = new FormData()
+
+    formData.append(
+      "message",
+      `${title} \n ${toPlainText(content)} \n https://good-talk.vercel.app/posts/${slug.current}`
+    )
+
+    const res = await fetch("https://notify-api.line.me/api/notify", {
+      method: "POST",
+      headers: {
+        // unnecessary content configuration, cause browser will automatically set a boundary parameter for data split
+        // "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_LINE_NOTIFY_TOKEN!}`,
+      },
+      body: formData,
+    })
+
+    const data = await res.json()
+
+    revalidatePath("/")
+    return NextResponse.json({
+      status: 200,
+      revalidated: true,
+      now: Date.now(),
+      data,
+    })
+  } catch (error: any) {
+    console.error(error)
+    return new Response(error.message, { status: 500 })
+  }
 }
