5fc2e5bda7bd5f8368bf74e381625049b33384d7
davelin18yufan <davelin30630@gmail.com>
2024-06-17 17:21:49 +0800

    [feat] let date scroll to current date at loading

--- app/(record)/DateFilter.tsx
+++ app/(record)/DateFilter.tsx
@@ -1,9 +1,29 @@
 "use client"
-import React, { useState } from "react"
+import { cn } from "@/utils"
+import React, { useEffect, useState, useRef } from "react"
 
-const DateFilter = () => {
+export function DateContainer({
+  layout,
+  children,
+}: {
+  layout: string
+  children: React.ReactNode
+}) {
+  const today = new Date(Date.now()).toISOString().split("T")[0]
+  const [selectedDate, setSelectedDate] = useState<string | null>(today)
+  
+  return (
+    <div className={layout}>
+      <DateFilter today={today} />
+      {children}
+    </div>
+  )
+}
+
+const DateFilter = ({ today }: { today: string }) => {
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const generateDatesForMonth = (month: number, year: number): string[] => {
     let date = new Date(year, month, 1)
@@ -12,7 +32,7 @@ const DateFilter = () => {
       date = new Date(year, month, date.getDate() + 1)
       dates.push(new Date(date).toISOString().split("T")[0])
     }
-    
+
     return dates
   }
 
@@ -43,32 +63,51 @@ const DateFilter = () => {
     return `${month}/${day}`
   }
 
+  useEffect(() => {
+    const todayButton = document.getElementById(today)
+    const container = containerRef.current
+
+    if (todayButton && container) {
+      const containerRect = container.getBoundingClientRect()
+      const buttonRect = todayButton.getBoundingClientRect()
+      const scrollOffset =
+        buttonRect.left +
+        buttonRect.width / 2 -
+        containerRect.left -
+        containerRect.width / 2
+
+      container.scrollTo({
+        left: scrollOffset,
+        behavior: "smooth",
+      })
+    }
+  }, [today])
+
   return (
     <div className="p-4 bg-white rounded-md shadow-md mb-4">
       <div className="flex justify-between items-center mb-2">
-        <button
-          onClick={handlePrevMonth}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
+        <button onClick={handlePrevMonth} className="dateBtn py-1">
           上月
         </button>
         <span className="text-lg font-semibold">{`${currentYear}-${String(currentMonth + 1).padStart(2, "0")}`}</span>
-        <button
-          onClick={handleNextMonth}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
+        <button onClick={handleNextMonth} className="dateBtn py-1">
           下月
         </button>
       </div>
-      <div className="flex space-x-2 overflow-x-auto">
-        {dates.map((date, index) => (
-          <button
-            key={index}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            {formatDate(date)}
-          </button>
-        ))}
+      <div className="flex space-x-2 overflow-x-auto" ref={containerRef}>
+        {dates.map((date, index) => {
+          const isToday = today === date
+
+          return (
+            <button
+              key={index}
+              id={date}
+              className={cn("dateBtn", isToday && "bg-white text-black")}
+            >
+              {formatDate(date)}
+            </button>
+          )
+        })}
       </div>
     </div>
   )
